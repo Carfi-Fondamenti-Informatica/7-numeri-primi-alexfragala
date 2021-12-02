af078cb609af6b2689878c7c8d0dd12bdf1689cc
alexfragala <91506326+alexfragala@users.noreply.github.com>
2021-12-02 02:46:15 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,11 @@
 
+//
+// Created by Alex Fragalà on 01/12/21.
+//
+
+#ifndef ESERCITAZIONE7_LIB_H
+#define ESERCITAZIONE7_LIB_H
+
+void numprimo(int a, int i, int &conta);
+
+#endif //ESERCITAZIONE7_LIB_H
