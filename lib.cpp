57fad1353f804a2e8dc818efa65a5238f87358e3
alexfragala <91506326+alexfragala@users.noreply.github.com>
2021-12-02 02:45:54 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,19 @@
+//
+// Created by Alex Fragalà on 01/12/21.
+//
 
+#include <iostream>
+#include "lib.h"
+
+void numprimo(int a, int i, int &conta){
+    int div=0;
+    i++;
+    if(a>1 and a>i){
+        div = a%i;
+        if(div==0){
+            conta++;
+        }else{
+            numprimo(a,i, conta);
+        }
+    }
+}
